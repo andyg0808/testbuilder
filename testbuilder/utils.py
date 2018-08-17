0d4542b41f8b15cbd155932ff3770f1d53e844f7
Andrew Gilbert <andrewg800@gmail.com>
2018-08-17 13:35:28 -0700

    Add `pipe_print` function
    This prints out a value coming through a pipeline, along with a nice
    message explaining where it is called from. It's useful for inspecting
    data in pipelines.

--- testbuilder/utils.py
+++ testbuilder/utils.py
@@ -1,4 +1,8 @@
 import ast
+import inspect
+from pprint import pprint
+from typing import Any
+
 from termcolor import cprint
 
 
@@ -18,3 +22,17 @@ def crash(reason: str = ""):
     if reason:
         raise RuntimeError("Crashing because" + reason)
     raise RuntimeError("Crashing!")
+
+
+def pipe_print(value: Any, message: str = "") -> Any:
+    if message == "":
+        stack = inspect.stack()
+        assert len(stack) > 0
+        caller = stack[0]
+        print(f"Pipe printing for {caller.function}")
+        del caller
+    else:
+        print(message)
+    pprint(value)
+    print("End pipe print")
+    return value
