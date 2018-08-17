63fcf9fce8f8095024b175c77b86a6b8a4ea0165
Andrew Gilbert <andrewg800@gmail.com>
2018-08-17 13:34:16 -0700

    Clean up in `utils`

--- testbuilder/utils.py
+++ testbuilder/utils.py
@@ -1,9 +1,14 @@
 import ast
 from termcolor import cprint
 
+
 def print_locations(node):
     for i in ast.walk(node):
-        if not hasattr(i, 'lineno') or not hasattr(i, 'col_offset'):
-            cprint(i, 'red')
+        if not hasattr(i, "lineno") or not hasattr(i, "col_offset"):
+            cprint(i, "red")
         else:
-            print("{} line={} col={}".format(i, getattr(i, 'lineno', 'none'), getattr(i, 'col_offset', 'none')))
+            print(
+                "{} line={} col={}".format(
+                    i, getattr(i, "lineno", "none"), getattr(i, "col_offset", "none")
+                )
+            )
