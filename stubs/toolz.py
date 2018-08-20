4ed16edea3f5cf0fd01c4d7993dcea464a17d998
Andrew Gilbert <andrewg800@gmail.com>
2018-08-20 15:22:55 -0700

    Add stub for mapcat

--- stubs/toolz.py
+++ stubs/toolz.py
@@ -1,4 +1,4 @@
-from typing import Callable, TypeVar
+from typing import Callable, Sequence, TypeVar
 
 
 def compose(*funcs: Callable) -> Callable:
@@ -10,4 +10,8 @@ U = TypeVar("U")
 
 
 def pipe(data: T, *funcs: Callable) -> U:
+    ...
+
+
+def mapcat(func: Callable[[T], Sequence[U]], seqs: Sequence[T]) -> Sequence[U]:
     ...
