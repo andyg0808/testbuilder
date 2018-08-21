f59d2dc8a71ea2079672534cf70f600275a3e378
Andrew Gilbert <andrewg800@gmail.com>
2018-08-21 16:41:32 -0700

    Write a few stubs for typechecking uses of `logbook`

---
+++ stubs/logbook.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+
+def critical(*args: Any, **kwargs: Any) -> None:
+    ...
+
+
+def warn(*args: Any, **kwargs: Any) -> None:
+    ...
+
+
+def warning(*args: Any, **kwargs: Any) -> None:
+    ...
+
+
+def notice(*args: Any, **kwargs: Any) -> None:
+    ...
+
+
+def debug(*args: Any, **kwargs: Any) -> None:
+    ...
+
+
+def info(*args: Any, **kwargs: Any) -> None:
+    ...
