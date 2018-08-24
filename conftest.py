8a658d2a0d59ed8a555abc05a0dff89a4c2ca9d8
Andrew Gilbert <andrewg800@gmail.com>
2018-08-24 15:59:27 -0700

    Write hook functions to ignore low-priority errors from logbook

--- conftest.py
+++ conftest.py
@@ -1,2 +1,15 @@
-def pytest_sessionstart():
-    from logbook import 
+import os
+def pytest_configure(config):
+    global verbosity
+    verbosity = config.getoption('--verbose')
+
+
+def pytest_runtest_logstart():
+    from logbook import StderrHandler, NullHandler
+    import logbook
+    null_handler = NullHandler()
+    null_handler.push_application()
+    stderr_handler = StderrHandler(level='WARNING')
+    stderr_handler.push_application()
+    if os.environ.get('VERBOSE', False) or verbosity:
+        StderrHandler().push_application()
