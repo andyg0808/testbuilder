b96bbd6403b43e91a2507b5dc2e343a470e087bc
Andrew Gilbert <andrewg800@gmail.com>
2018-08-24 10:16:58 -0700

    Add stubs for logbook

--- stubs/logbook.py
+++ stubs/logbook.py
@@ -5,6 +5,10 @@ def critical(*args: Any, **kwargs: Any) -> None:
     ...
 
 
+def error(*args: Any, **kwargs: Any) -> None:
+    ...
+
+
 def warn(*args: Any, **kwargs: Any) -> None:
     ...
 
@@ -23,3 +27,29 @@ def debug(*args: Any, **kwargs: Any) -> None:
 
 def info(*args: Any, **kwargs: Any) -> None:
     ...
+
+
+class Logger:
+    def __init__(self, name: str) -> None:
+        ...
+
+    def critical(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def error(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def warn(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def warning(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def notice(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def debug(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def info(self, *args: Any, **kwargs: Any) -> None:
+        ...
