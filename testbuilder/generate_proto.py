96ed37f568793eebe62490d407f8d05faf4b2a58
Andrew Gilbert <andrewg800@gmail.com>
2018-08-31 11:52:36 -0700

    Switch to only using new expression building code
    Old version has problems with `test_slice_on_condition`. The new
    version handles it fine (it doesn't mind slicing so there are no
    constraints). So we'll just use the new version exclusively now. We
    have the tests, so this shouldn't be too risky.

--- testbuilder/generate_proto.py
+++ testbuilder/generate_proto.py
@@ -76,12 +76,12 @@ def filter_inputs(function: FunctionDef, inputs: Solution) -> Solution:
 
 def get_expression_at_depth(depth: int, funcstmt: FuncStmt) -> Expression:
     variables = funcstmt.statement.get_slice_variables()
-    block_tree = build_tree(funcstmt.function)
-    flowgraph = block_tree.inflate(funcstmt.statement)
+    # block_tree = build_tree(funcstmt.function)
+    # flowgraph = block_tree.inflate(funcstmt.statement)
     builder = ExpressionBuilder(
         depth, funcstmt.statement.lineno, funcstmt.statement.lines()
     )
-    return builder.get_expression(variables, flowgraph)
+    return builder.get_expression(variables, funcstmt.function)
 
 
 get_expression = partial(get_expression_at_depth, 2)
