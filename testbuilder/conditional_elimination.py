721b95bc261e25accefe3eaedd0ea919b9dc575d
Andrew Gilbert <andrewg800@gmail.com>
2018-09-04 13:00:15 -0700

    Add logging to ConditionalElimination

--- testbuilder/conditional_elimination.py
+++ testbuilder/conditional_elimination.py
@@ -1,12 +1,18 @@
 from . import ssa_basic_blocks as sbb
 from .visitor import UpdateVisitor
-
+from logbook import Logger, StderrHandler
+log = Logger('conditional_elimination')
+StderrHandler(
+    level="INFO", filter=lambda r, h: r.channel == "conditional_elimination"
+).push_application()
 
 class ConditionalElimination(UpdateVisitor):
     def visit_Conditional(self, cond: sbb.Conditional) -> sbb.BasicBlock:
         if isinstance(cond.true_branch, sbb.TrueBranch) and isinstance(
             cond.false_branch, sbb.FalseBranch
         ):
+            log.notice(f"Discarding conditional on lines"
+                       f" {cond.first_line}–{cond.last_line} because it is empty")
             return self.visit(cond.parent)
         else:
             return super().generic_visit(cond)
