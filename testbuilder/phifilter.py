108c4c1a4022c4d67d0cea7a97adbe3771a36a27
Andrew Gilbert <andrewg800@gmail.com>
2018-09-07 11:27:32 -0700

    Phi-filter multi-path nodes correctly

--- testbuilder/phifilter.py
+++ testbuilder/phifilter.py
@@ -1,9 +1,9 @@
-from typing import Optional, Set, Tuple
+from copy import copy
+from typing import Optional
 
 from . import nodetree as n, ssa_basic_blocks as sbb
+from .target_manager import TargetManager
 from .visitor import UpdateVisitor
-
-NameSet = Set[Tuple[str, int]]
 
 
 class PhiFilterer(UpdateVisitor):
@@ -13,23 +13,55 @@ class PhiFilterer(UpdateVisitor):
     """
 
     def visit_Request(self, request: sbb.Request) -> sbb.Request:
-        return sbb.Request(module=request.module, code=self.visit(request.code, set()))
+        return sbb.Request(
+            module=request.module, code=self.visit(request.code, TargetManager())
+        )
 
-    def visit_Name(self, name: n.Name, seen: NameSet) -> n.Name:
-        seen.add((name.id, name.set_count))
+    def visit_Name(self, name: n.Name, seen: TargetManager) -> n.Name:
+        seen.add(name.id, name.set_count)
         return name
 
-    def visit_PhiSet(self, phiset: n.PhiSet, seen: NameSet) -> Optional[n.PhiSet]:
+    def visit_PhiSet(self, phiset: n.PhiSet, seen: TargetManager) -> Optional[n.PhiSet]:
         target = phiset.target
         if (target.id, target.set_count) in seen:
             return phiset
         else:
             return None
 
-    # TODO: Ensure that multi-path nodes get separate variable batches
-    # for each path.
+    def visit_Conditional(
+        self, cond: sbb.Conditional, seen: TargetManager
+    ) -> sbb.Conditional:
+        true_seen = copy(seen)
+        false_seen = copy(seen)
+        true_branch = self.visit(cond.true_branch, true_seen)
+        false_branch = self.visit(cond.false_branch, false_seen)
+        seen.update(true_seen | false_seen)
+        return sbb.Conditional(
+            number=cond.number,
+            first_line=cond.first_line,
+            last_line=cond.last_line,
+            parent=self.visit(cond.parent, seen),
+            true_branch=true_branch,
+            false_branch=false_branch,
+        )
 
-    def visit_Code(self, code: sbb.Code, seen: NameSet) -> sbb.BasicBlock:
+    def visit_Loop(self, loop: sbb.Loop, seen: TargetManager) -> sbb.Loop:
+        loops = []
+        post_seen = TargetManager()
+        for l in loop.loops:
+            iter_seen = copy(seen)
+            loops.append(self.visit(l, iter_seen))
+            post_seen.merge(iter_seen)
+        seen.update(post_seen)
+        return sbb.Loop(
+            number=loop.number,
+            first_line=loop.first_line,
+            last_line=loop.last_line,
+            parent=self.visit(loop.parent, seen),
+            loops=loops,
+        )
+
+    def visit_Code(self, code: sbb.Code, seen: TargetManager) -> sbb.BasicBlock:
         visited = [self.visit(l, seen) for l in code.code]
         filtered = [l for l in visited if l is not None]
         parent = self.visit(code.parent, seen)
