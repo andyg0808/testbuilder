921f9bddd9955c6e3514afa15b4f2057bcb83819
Andrew Gilbert <andrewg800@gmail.com>
2018-09-11 11:52:02 -0700

    Make `line_splitter` strip duplicate and negative line numbers

--- testbuilder/line_splitter.py
+++ testbuilder/line_splitter.py
@@ -1,13 +1,20 @@
 """
 Produces a list of line numbers present in an SSA AST
 """
+from functools import partial
 from typing import List
+
+from toolz import pipe
 
 from . import nodetree as n, ssa_basic_blocks as sbb
 from .visitor import GatherVisitor
 
 
 class LineSplitter(GatherVisitor[int]):
+    def visit_Module(self, node: sbb.Module) -> List[int]:
+        _filter_high = partial(filter, lambda i: i >= 0)
+        return pipe(node, self.generic_visit, set, _filter_high, list)
+
     def visit_Stmt(self, node: n.stmt) -> List[int]:
         return [node.line]
 
