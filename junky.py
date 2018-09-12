4b3f0772a5f7f5118a6a63663622c2c78467730b
Andrew Gilbert <andrewg800@gmail.com>
2018-09-12 00:58:10 -0700

    Add an additional case to generate tests on
    This one has lots of junk in the functions.

---
+++ junky.py
@@ -0,0 +1,12 @@
+def JunkyFunction(askjd):
+    askjd
+    askjd * 42
+    12
+
+
+def anotherJunkyF27364(lkjafskjdsskj):
+    JunkyFunction(lkjafskjdsskj)
+    JunkyFunction(12341)
+    UGYYG = JunkyFunction(42)
+    JunkyFunction(UGYYG)
+    return 22
