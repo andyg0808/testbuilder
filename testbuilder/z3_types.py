096e3dd211d9bdd10789f25212c43bd0180fb98d
Andrew Gilbert <andrewg800@gmail.com>
2018-10-24 13:04:09 -0700

    Add additional simplifications to `bool_`* functions
    Eliminating the basic, silly things means we can afford to generate
    slightly sillier expressions other places and clean them up when
    combining them. Also, it makes testing easier, because the output is
    clean.

--- testbuilder/z3_types.py
+++ testbuilder/z3_types.py
@@ -494,15 +494,19 @@ class MoreMagic:
 
 
 def bool_not(expr: z3.Bool) -> z3.Bool:
+    if z3.eq(expr, BOOL_TRUE):
+        return BOOL_FALSE
+    if z3.eq(expr, BOOL_FALSE):
+        return BOOL_TRUE
     return z3.Not(expr)
 
 
 def bool_or(exprs: Iterable[z3.Bool]) -> z3.Bool:
-    return _simplify_logical(exprs, z3.Or)
+    return _simplify_logical(exprs, True, z3.Or)
 
 
 def bool_and(exprs: Iterable[z3.Bool]) -> z3.Bool:
-    return _simplify_logical(exprs, z3.And)
+    return _simplify_logical(exprs, False, z3.And)
 
 
 def bool_all(exprs: Iterable[z3.Bool]) -> z3.Bool:
@@ -517,14 +521,34 @@ def bool_any(exprs: Iterable[z3.Bool]) -> z3.Bool:
     return bool_or(exprs)
 
 
+BOOL_TRUE = z3.BoolVal(True)
+BOOL_FALSE = z3.BoolVal(False)
+
+
 def bool_true() -> Expression:
-    return z3.BoolVal(True)
+    return BOOL_TRUE
 
 
 def _simplify_logical(
-    exprs: Iterable[z3.Bool], function: Callable[..., z3.Bool]
+    exprs: Iterable[z3.Bool], eliminate: bool, function: Callable[..., z3.Bool]
 ) -> z3.Bool:
     exprs = list(exprs)
+    # `eliminate_bool` is the value which, if present in exprs, would
+    # make the entire expression evaluate to itself. For `and`, this
+    # is `False`; for `or`, this is `True`.
+    eliminate_bool = z3.BoolVal(eliminate)
+    # Similarly to `eliminate_bool`, `combine_bool` is the value
+    # which, if all the elements of `exprs` are it, would make the
+    # whole expression equal to itself. For `and`, this is `True`; for
+    # `or`, this is `False`. Note that, if not every value in `exprs`
+    # is this value, any occurances of it can simply be deleted
+    # without changing the value of the expression.
+    combine_bool = z3.BoolVal(not eliminate)
+    if any(z3.eq(eliminate_bool, e) for e in exprs):
+        return eliminate_bool
+    if all(z3.eq(combine_bool, e) for e in exprs):
+        return combine_bool
+    exprs = [e for e in exprs if not z3.eq(combine_bool, e)]
     if len(exprs) > 1:
         return function(*exprs)
     elif len(exprs) == 1:
