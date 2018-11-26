d9f78a83d2fc97863c30b28c09cdaf3c3bc4456e
Andrew Gilbert <andrewg800@gmail.com>
2018-11-26 11:01:32 -0800

    Generalize `check_expression` function

--- testbuilder/expression_builder_test.py
+++ testbuilder/expression_builder_test.py
@@ -6,64 +6,13 @@ from toolz import pipe
 
 import z3
 
+from .check_expression import ExpressionChecker
 from .expression_builder import get_expression
 from .type_builder import TypeBuilder
 from .variable_expander import expand_variables
 from .z3_types import diff_expression, print_diff
 
-
-def check_expression(
-    code_string,
-    expected,
-    line=-1,
-    simplify=False,
-    write_tree="",
-    depth: int = 1,
-    overall=False,
-):
-    """
-    Args:
-        code_string: A string of code to create an expression for
-        expected: The expected expression resulting from the string of code
-        line: The line number on which to slice.
-        simplify: Whether to run z3.simplify on the example and actual output
-                  before comparing.
-        write_tree: If nonempty, a filename to which to write the basic block
-                    tree generated while converting the expression.
-        depth: The loop unrolling depth to use
-        overall: Whether to generate an expression which does not use a
-                 particular slice of the code.
-
-        The overall option forces the expression to include all exit points
-        from the function. It still uses the line number to choose which code
-        to include, but control structures will be included for all exit
-        points. Currently does nothing.
-    """
-    builder = TypeBuilder()
-    builder.wrappers()
-    Registrar = builder.build()
-
-    if isinstance(expected, str):
-        expected = expand_variables(expected, Registrar)
-    _get_expression = partial(get_expression, Registrar, line, depth=depth)
-    test_data = pipe(code_string.strip(), ast.parse, _get_expression)
-    if test_data is None:
-        expr = None
-    else:
-        expr = test_data.expression
-    print("expected  ", expected)
-    print("expression", expr)
-    if simplify:
-        expected = z3.simplify(expected)
-        expr = z3.simplify(expr)
-    if expected is None:
-        assert expr is None
-    else:
-        assert expr is not None
-        diff = diff_expression(expected, expr)
-        if diff is not None:
-            print_diff(diff)
-        assert diff is None
+check_expression = ExpressionChecker(lambda b: b.wrappers())
 
 
 def test_basic_call():
--- testbuilder/pair_test.py
+++ testbuilder/pair_test.py
@@ -1,75 +1,8 @@
-import ast
-from functools import partial
-
 import pytest
-from toolz import pipe
 
-import z3
+from .check_expression import ExpressionChecker
 
-from .expression_builder import get_expression
-from .type_builder import TypeBuilder
-from .variable_expander import expand_variables
-from .z3_types import Reference, diff_expression, print_diff
-
-
-def check_expression(
-    code_string,
-    expected,
-    line=-1,
-    simplify=False,
-    write_tree="",
-    depth: int = 1,
-    overall=False,
-):
-    """
-    Args:
-        code_string: A string of code to create an expression for
-        expected: The expected expression resulting from the string of code
-        line: The line number on which to slice.
-        simplify: Whether to run z3.simplify on the example and actual output
-                  before comparing.
-        write_tree: If nonempty, a filename to which to write the basic block
-                    tree generated while converting the expression.
-        depth: The loop unrolling depth to use
-        overall: Whether to generate an expression which does not use a
-                 particular slice of the code.
-
-        The overall option forces the expression to include all exit points
-        from the function. It still uses the line number to choose which code
-        to include, but control structures will be included for all exit
-        points. Currently does nothing.
-    """
-    builder = TypeBuilder()
-    builder.wrappers().references().structures()
-    Registrar = builder.build()
-
-    if isinstance(expected, str):
-        store_sort = z3.ArraySort(Reference, Registrar.reftype)
-        local_vals = {
-            "store": z3.Const("store", store_sort),
-            "store_1": z3.Const("store_1", store_sort),
-            "store_2": z3.Const("store_2", store_sort),
-            "Reference": lambda i: Reference.Reference(i),
-        }
-        expected = expand_variables(expected, Registrar, local_vals=local_vals)
-    _get_expression = partial(get_expression, Registrar, line, depth=depth)
-    test_data = pipe(code_string.strip(), ast.parse, _get_expression)
-    if test_data is None:
-        expr = None
-    else:
-        expr = test_data.expression
-    print("expected  ", expected)
-    print("expression", expr)
-    if simplify:
-        expected = z3.simplify(expected)
-        expr = z3.simplify(expr)
-    if expected is None:
-        assert expr is None
-    else:
-        diff = diff_expression(expected, expr)
-        if diff is not None:
-            print_diff(diff)
-            raise AssertionError("Expressions differ")
+check_expression = ExpressionChecker(lambda b: b.wrappers().references().structures())
 
 
 @pytest.mark.skip(
