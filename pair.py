a455aea98eda0ed1ad1eacf27031da0604bfe465
Andrew Gilbert <andrewg800@gmail.com>
2019-01-09 20:37:16 -0800

    More extensive pair test

--- pair.py
+++ pair.py
@@ -1,3 +1,11 @@
-def basic_pair_use(a):
-    a.left = a.left + 32
+def basic_pair_use(first, a):
+    if first == 0 and a.left == 1:
+        a.left = a.left + 32
+        return a.left
+    else:
+        return 5
+
+
+def simple_inferred_pair(a):
+    a.left += 32
     return a
