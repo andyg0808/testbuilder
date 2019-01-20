5446487bd2bc905ee87a3b01644d0e0cb186d47c
Andrew Gilbert <andrewg800@gmail.com>
2019-01-19 23:16:41 -0800

    Add additional time before timeout on Expect tests

--- testing.tcl
+++ testing.tcl
@@ -1,3 +1,4 @@
 proc fail msg {puts stderr $msg; exit 6;}
 proc abort msg {puts stderr $msg; exit 7;}
+set timeout 40
 expect_after default {fail "Got <$expect_out(buffer)> but expected something else."}
