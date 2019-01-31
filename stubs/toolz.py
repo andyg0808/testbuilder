785fa14dd853b52316b71db9e9f1448ad1e97269
Andrew Gilbert <andrewg800@gmail.com>
2019-01-31 00:03:30 -0800

    Start work on proper typing for pipe
    This technique is being used by
    https://github.com/python/typeshed/blob/ad803e1caa9764e8778ce558fb99e7d4cca3cda9/stdlib/2and3/builtins.pyi;
    it seems that when https://github.com/python/typing/issues/193
    happens, it should allow us to describe this cleanly.

--- stubs/toolz.py
+++ stubs/toolz.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable, List, Mapping, Sequence, TypeVar
+from typing import Callable, Iterable, List, Mapping, Sequence, TypeVar, overload
 
 
 def compose(*funcs: Callable) -> Callable:
@@ -8,8 +8,48 @@ def compose(*funcs: Callable) -> Callable:
 T = TypeVar("T")
 U = TypeVar("U")
 
+# As seen in https://github.com/python/typeshed/blob/ad803e1caa9764e8778ce558fb99e7d4cca3cda9/stdlib/2and3/builtins.pyi
+_T1 = TypeVar("_T1")
+_T2 = TypeVar("_T2")
+_T3 = TypeVar("_T3")
+_T4 = TypeVar("_T4")
+_T5 = TypeVar("_T5")
+_T6 = TypeVar("_T6")
+_T7 = TypeVar("_T7")
 
-def pipe(data: T, *funcs: Callable) -> U:
+
+def pipe(data: T, funcs: Callable[[T], _T1]) -> _T1:
+    ...
+
+
+@overload
+def pipe(data: T, func1: Callable[[T], _T1], func2: Callable[[_T1], _T2]) -> _T2:
+    ...
+
+
+@overload
+def pipe(
+    data: T,
+    func1: Callable[[T], _T1],
+    func2: Callable[[_T1], _T2],
+    func3: Callable[[_T2], _T3],
+) -> _T3:
+    ...
+
+
+@overload
+def pipe(
+    data: T,
+    func1: Callable[[T], _T1],
+    func2: Callable[[_T1], _T2],
+    func3: Callable[[_T2], _T3],
+    func4: Callable[[_T3], _T4],
+) -> _T4:
+    ...
+
+
+@overload
+def pipe(data: T, funcs: Callable) -> U:
     ...
 
 
