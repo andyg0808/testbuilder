d5f97fb7d2389db806ff97456be867b3019a7298
Andrew Gilbert <andrewg800@gmail.com>
2019-02-07 10:37:49 -0800

    Add Z3 path to `sys.path` during startup
    This is much simpler to get working, as it is the equivalent of what
    we were doing before with the virtualenv.

--- _z3config.py
+++ _z3config.py
@@ -1,10 +1,12 @@
 import builtins
 import os
+import sys
 from pathlib import Path
 
 root = Path(__file__).parent
 
 builtins.Z3_LIB_DIRS = [root / "z3_source" / "build"]
+sys.path.append(str(root / "z3_source" / "build" / "python"))
 
 if os.environ.get("Z3_ROOT_PRINT", False):
     print(builtins.Z3_LIB_DIRS)
